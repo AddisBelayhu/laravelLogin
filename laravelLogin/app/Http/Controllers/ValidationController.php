b8ba6e0f1f738e73c07b8e7e43bdce1fdbfd6424
AddisBelayhu <addisgithub@gmail.com>
2023-06-16 15:05:37 +0300

    validate

--- laravelLogin/app/Http/Controllers/ValidationController.php
+++ laravelLogin/app/Http/Controllers/ValidationController.php
@@ -3,8 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Controller;
+use App\Http\routes;
 
 class ValidationController extends Controller
 {
-    //
+    public function showform(){
+        return view('login');
+    }
+
+    public function validateform(Request $request){
+        print_r($request->all());
+        $this->validate($request,[
+            'username'=>'required|max:8',
+            'password'=>'required'
+        ]);
+    }
 }
